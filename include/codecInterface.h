906a563b0691a84e31e222f5ddf7b5ae4dc2f397
Dekevich, Nikita <nikita@dekevich.com>
2016-08-04 18:22:48 +0300

    working on codec interface /opus realization

--- include/codecInterface.h
+++ include/codecInterface.h
@@ -1,5 +1,8 @@
 #include <stdint.h>
 #include <unordered_map>
+#include <boost/any.hpp>
+
+
 
 class codecInterface
 {
@@ -7,13 +10,19 @@ class codecInterface
 		codecInterface();
 		virtual ~codecInterface();
 
+		virtual int createEncoder();
+		virtual int createDecoder();
+
+		virtual int destroyEncoder();
+		virtual int destroyDecoder();
+
 
 		virtual void initialize();
-		virtual int getCodecCode();
+		virtual int getCodecType();
 
 		virtual uint8_t code(uint8_t rawData);
 		virtual uint8_t decode(uint8_t codedData);
-		 
+		virtual int codecFunction(std::list<boost::any> arguments);
 
 
 		std::unordered_map < std::string, int > settings;
--- src/opusInterface.cpp
+++ src/opusInterface.cpp
@@ -1,6 +1,7 @@
 #include "codecInterface.h"
 #include "unordered_map"
 #include <include/opus.h>
+#include <boost/any.hpp>
 
 class opusCodec :public codecInterface
 {
@@ -8,11 +9,38 @@ class opusCodec :public codecInterface
 		encoder = opus_encoder_create(sets["fs"], sets["channels"], sets["application"], error_encoder);
 		decoder = opus_decoder_create(sets["fs"] , sets ["channels"], error_decoder);
 	}
-
-	OpusEncoder* intialization(std::unordered_map<std::string, int> set){
-		settings = set;
-		return 0;
+	~opusCodec() {
+		opus_encoder_destroy(encoder);
+		opus_decoder_destroy(decoder);
 	}
+
+	int createEncoder(std::unordered_map<std::string, int> sets) {
+		encoder = opus_encoder_create(sets["fs"], sets["channels"], sets["application"], error_encoder);
+		return *error_encoder;
+	}
+
+	int createDecoder(std::unordered_map<std::string, int> sets) {
+		opus_decoder_destroy(decoder);
+		decoder = opus_decoder_create(sets["fs"], sets["channels"], error_decoder);
+		return *error_decoder;
+	}
+	
+	int destroyEncoder() {
+		opus_encoder_destroy(encoder);
+	}
+	
+	int destroyDecoder() {
+		opus_decoder_destroy(decoder);
+	}
+	
+	virtual int codecFunction(std::vector<boost::any> arguments) {
+		if (arguments.size() < 1) {
+			return -1;
+		}
+///////////////HERE!!
+	}
+
+
 
 	std::vector<uint8_t> code(std::vector<uint8_t>){
 	}
